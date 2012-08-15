651d2f89c7b4200f00a07d90a5a16e36cbe7f90b
Brett Hughes <bretthughes12@optusnet.com.au>
2012-08-15 22:13:39 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footyshares::Application.config.secret_token = '057122f994d1d0218085b7f955302b641485b5c2d012f2244cba3245b48d8c46ca408e97f4eebdfa099ab8dbf009a5d84e24df0ea9f75fef7c02108dbb657ba7'
