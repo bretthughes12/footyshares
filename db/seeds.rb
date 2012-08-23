ae4a782c1589719c01cfdcf8d5e7b3e8ea951a5e
Brett Hughes <bretthughes12@optusnet.com.au>
2012-08-23 23:26:37 +1000

    Created initial database entries via seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+# Create an initial admin user so that the site can be administered
+user = User.new(:login => 'admin',
+                :name => 'Admin User',
+                :password => 'secret',
+                :password_confirmation => 'secret',
+                :email => 'bretthughes12@gmail.com',
+                :starting_shares => 50 )
+user.admin = true
+user.save
+
+# Create each round
+round_1 = Round.create(:name => 'Week 1')
+round_2 = Round.create(:name => 'Week 2')
+round_3 = Round.create(:name => 'Week 3')
+round_4 = Round.create(:name => 'Week 4')
+
+# Create each of the matches to be played
+Match.create([{ :name => '1st Qualifying Final', :round_id => round_1.id },
+              { :name => '2nd Qualifying Final', :round_id => round_1.id },
+              { :name => '1st Elimination Final', :round_id => round_1.id },
+              { :name => '2nd Elimination Final', :round_id => round_1.id },
+              { :name => '1st Semi Final', :round_id => round_2.id },
+              { :name => '2nd Semi Final', :round_id => round_2.id },
+              { :name => '1st Preliminary Final', :round_id => round_3.id },
+              { :name => '2nd Preliminary Final', :round_id => round_3.id },
+              { :name => 'Grand Final', :round_id => round_4.id, :allow_only_one_team => true },])
