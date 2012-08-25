7aa52ac6ff46560b688045809b168c15ea157f9c
Brett Hughes <bretthughes12@optusnet.com.au>
2012-08-25 23:41:42 +1000

    Attempt to fix asset precompile error on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Footyshares::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( ie6.css ie7.css )  
+  config.assets.initialize_on_precompile = false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
