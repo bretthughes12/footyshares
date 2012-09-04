b162f3a585791466b7d9aeae74521ab255d88548
Brett Hughes <bretthughes12@optusnet.com.au>
2012-09-04 21:42:44 +1000

    Prevent new signups after the first round cutoff

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,22 +1,30 @@
 class SignupsController < ApplicationController
   def new
+    @round = Round.current
     @user = User.new
   end
 
   def create
-    @user = User.new(params[:signup])
+    @round = Round.current
 
-    respond_to do |format|
-      if @user.save
-        flash[:notice] = 'Thank you for your time. Check your inbox for a confirmation email. You are now logged in'
-        UserMailer.welcome(@user).deliver       
-        format.html do
-          session[:user_id] = @user.id
-          redirect_to root_url
+    if @round.open
+      @user = User.new(params[:signup])
+  
+      respond_to do |format|
+        if @user.save
+          flash[:notice] = 'Thank you for your time. Check your inbox for a confirmation email. You are now logged in'
+          UserMailer.welcome(@user).deliver       
+          format.html do
+            session[:user_id] = @user.id
+            redirect_to root_url
+          end
+        else
+          format.html { render action: :new }
         end
-      else
-        format.html { render action: "new" }
       end
-    end
+    else
+      flash[:notice] = 'The competition has already started. New signups are now closed.'
+      render action: :new
+    end  
   end
 end
