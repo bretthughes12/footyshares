5fb2ea21ffdbc85c8d890c135b7df69ae94ea3d6
Brett Hughes <bretthughes12@optusnet.com.au>
2014-10-01 21:26:10 +1000

    Assets tweaks for Production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( ie6.css ie7.css )  
 
 # Enable the asset pipeline
 Rails.application.config.assets.enabled = true
